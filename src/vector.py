2973f14a95b577064277e69337714bfe8d61d556
Mikhail Romanov <mikhail.romanov@aalto.fi>
2025-02-17 16:00:27 +0200

    deleted get functions

--- src/vector.py
+++ src/vector.py
@@ -1,13 +1,6 @@
 class Vector:
 
     def __init__(self, x, y, z):
-        self.data = [x,y,z]
-
-    def x(self):
-        return self.data[0]
-    
-    def y(self):
-        return self.data[1]
-    
-    def z(self):
-        return self.data[2]
+        self.x = x 
+        self.y = y
+        self.z = z
