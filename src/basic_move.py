48d1aca7e4da5d11504604982e2eea26979a78a3
rasberry pi <mikhail.romanov@aalto.fi>
2025-03-24 06:52:24 +0000

    updated pins

--- src/basic_move.py
+++ src/basic_move.py
@@ -1,35 +1,28 @@
 from time import sleep
 import RPi.GPIO as gpio
 
-direction_pin   = 20
-pulse_pin       = 21
+direction_pins = [5, 12, 27] # brown
+pulse_pins      = [13, 16, 19] # white
 cw_direction    = 0 
 ccw_direction   = 1 
 
 gpio.setmode(gpio.BCM)
-gpio.setup(direction_pin, gpio.OUT)
-gpio.setup(pulse_pin, gpio.OUT)
-gpio.output(direction_pin,cw_direction)
+for direction_pin in direction_pins:
+    gpio.setup(direction_pin, gpio.OUT)
+    gpio.output(direction_pin,cw_direction)
+for pulse_pin in pulse_pins:
+    gpio.setup(pulse_pin, gpio.OUT)
 
 try:
     while True:
-        print('Direction CW')
-        sleep(.5)
-        gpio.output(direction_pin,cw_direction)
-        for x in range(200):
-            gpio.output(pulse_pin,gpio.HIGH)
-            sleep(.001)
-            gpio.output(pulse_pin,gpio.LOW)
-            sleep(.0005)
+        for x in range(1600):
+            for pin in pulse_pins:
+                gpio.output(pin,gpio.HIGH)
+            sleep(1e-5)
 
-        print('Direction CCW')
-        sleep(.5)
-        gpio.output(direction_pin,ccw_direction)
-        for x in range(200):
-            gpio.output(pulse_pin,gpio.HIGH)
-            sleep(.001)
-            gpio.output(pulse_pin,gpio.LOW)
-            sleep(.0005)
+            for pin in pulse_pins:
+                gpio.output(pin,gpio.LOW)
+            sleep(1e-5)
 
 except KeyboardInterrupt:
     gpio.cleanup()
