51c5620b4c2744df7516bf3b91bc2bbd858392e6
Mikhail Romanov <mikhail.romanov@aalto.fi>
2025-03-27 17:18:22 +0200

    converted to gripzero

--- src/basic_move.py
+++ src/basic_move.py
@@ -1,36 +1,39 @@
 from time import sleep
-import RPi.GPIO as gpio
 import threading
+from gpiozero import DigitalOutputDevice
 from cooling import FanController
 
-direction_pins = [26, 4, 19] # brown
-pulse_pins      = [13, 27, 20] # white
-cw_direction    = 0 
-ccw_direction   = 1 
+# GPIO pin assignments
+direction_pins = [26, 4, 19]  # brown wires
+pulse_pins     = [13, 27, 20] # white wires
 
-gpio.setmode(gpio.BCM)
-for direction_pin in direction_pins:
-    gpio.setup(direction_pin, gpio.OUT)
-    gpio.output(direction_pin,cw_direction)
-for pulse_pin in pulse_pins:
-    gpio.setup(pulse_pin, gpio.OUT)
+cw_direction  = False
+ccw_direction = True
 
-# Create FanController instance
-# And run the fan controller in a separate thread
+# Setup gpiozero devices
+direction_outputs = [DigitalOutputDevice(pin) for pin in direction_pins]
+pulse_outputs     = [DigitalOutputDevice(pin) for pin in pulse_pins]
+
+# Set initial direction (clockwise)
+for direction in direction_outputs:
+    direction.value = cw_direction
+
+# Start the fan controller in a separate thread
 cooler = FanController()
 fan_thread = threading.Thread(target=cooler.run, daemon=True)
 fan_thread.start()
 
 try:
     while True:
-        for x in range(1600):
-            for pin in pulse_pins:
-                gpio.output(pin,gpio.HIGH)
+        for _ in range(1600):
+            for pulse in pulse_outputs:
+                pulse.on()
             sleep(1e-5)
-
-            for pin in pulse_pins:
-                gpio.output(pin,gpio.LOW)
+            for pulse in pulse_outputs:
+                pulse.off()
             sleep(1e-5)
 
 except KeyboardInterrupt:
-    gpio.cleanup()
+    print("Interrupted. Cleaning up.")
+    for pin in direction_outputs + pulse_outputs:
+        pin.close()
