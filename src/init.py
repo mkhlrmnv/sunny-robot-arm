88113e6373b995bc1bea0e7dbd7f6f645883a9c5
rasberry pi <mikhail.romanov@aalto.fi>
2025-04-16 10:35:12 +0100

    changed pinns and motor init

--- src/init.py
+++ src/init.py
@@ -5,7 +5,7 @@ from cooling import FanController
 import threading
 
 # [[dir, puls]] => [[brown, white]]
-pins = [[26, 13]]
+pins = [[27, 4]]
 
 cooler = FanController()
 fan_thread = threading.Thread(target=cooler.run, daemon=True)
@@ -13,6 +13,6 @@ fan_thread.start()
 
 # initialize motors
 for pin in pins:
-    motor = Motor(pulse_pin=pin[1], dir_pin=pin[0], limit_pin=23, step_per_rev=1600, min_delay=0.001, max_delay=0.01, gear_ratio=1)
+    motor = motor3 = Motor(pulse_pin=27, dir_pin=4, limit_pin=23, gear_ratio=1)
     motor.init_motor(direction=1)
 
