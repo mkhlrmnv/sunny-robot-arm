ef913f7d70d75c73b269ce472ede8bd4fb1b73b7
Mikhail Romanov <mikhail.romanov@aalto.fi>
2025-04-24 20:13:08 +0300

    added second file for spinning motor joints

---
+++ src/spinning_joints.py
@@ -0,0 +1,109 @@
+from gpiozero import DigitalOutputDevice
+import time
+from gpiozero import Button
+from signal import pause
+
+class SpinningJoints:
+    def __init__(self, pulse_pin, dir_pin, limit_pin, step_per_rev=1600, gear_ratio=5, min_delay=1e-4, max_delay=1e-3):
+        """
+        Initialize the stepper motor with the given GPIO pins.
+
+        :param pulse_pin: GPIO pin for sending step pulses.
+        :param dir_pin: GPIO pin for setting the direction.
+        """
+        # asserts:)
+        assert isinstance(pulse_pin, int), "Pulse pin must be an integer."
+        assert isinstance(dir_pin, int), "Direction pin must be an integer."
+        assert isinstance(step_per_rev, int), "Steps per revolution must be an integer."
+        assert isinstance(gear_ratio, int), "Gear ratio must be an integer."
+        assert isinstance(min_delay, (int, float)), "Minimum delay must be a number."
+        assert isinstance(max_delay, (int, float)), "Maximum delay must be a number."
+        assert min_delay >= 0, "Minimum delay must be non-negative."
+        assert max_delay >= min_delay, "Maximum delay must be greater than or equal to minimum delay."
+        
+        self.pulse = DigitalOutputDevice(pulse_pin)
+        self.direction = DigitalOutputDevice(dir_pin)
+
+        self.limit_switch = Button(limit_pin, pull_up=True, bounce_time=0.0001)
+        self.limit_switch.when_pressed = lambda: (print("Button held"), 
+                                                setattr(self, 'init_pos', True))
+        self.limit_switch.when_released = lambda: (print("released"), 
+                                                    setattr(self, 'init_pos', False))
+
+        self.steps = 0
+        self.angle = 0
+        self.step_per_rev = step_per_rev
+        self.min_delay = min_delay
+        self.max_delay = max_delay
+        self.gear_ratio = gear_ratio
+
+        self.init_pos = False
+
+    def calc_delay(self, speed_percent):
+        if not (0 <= speed_percent <= 1):
+            raise ValueError("Speed percent must be between 0 and 1.")
+        return self.min_delay + (self.max_delay - self.min_delay) * (1 - speed_percent)
+    
+    def init_motor(self, direction=1):
+        while not self.init_pos:
+            self.step(direction=direction, speed=0.5)
+        self.reset_position()
+        print(f"Motor initialized, stop state {self.stop}")
+
+    def step(self, direction=1, speed=0.5):
+
+        if direction not in [-1, 1]:
+            raise ValueError("Direction must be 1 (forward) or -1 (backward).")
+
+        self.direction.value = 1 if direction == 1 else 0
+        delay = self.calc_delay(speed)
+
+        self.pulse.on()
+        time.sleep(delay)
+        self.pulse.off()
+        time.sleep(delay)
+        self.steps += direction
+        self.angle += direction * (360 / (self.step_per_rev * self.gear_ratio))
+        self.angle = round(self.angle, 3)
+
+    def move_by_angle(self, angle, speed=0.5):
+        if abs(angle) > 360:
+           raise ValueError("Angle must be between -360 and 360 degrees.")
+
+        angle_per_step = 360 / (self.step_per_rev * self.gear_ratio)
+        steps = int(angle / angle_per_step)
+        direction = 1 if angle > 0 else -1
+
+        for _ in range(abs(steps)):
+            self.step(direction=direction, speed=speed)
+
+    def move_to_angle(self, target_angle, speed=0.5):
+        if abs(target_angle) > 360:
+            raise ValueError("Target angle must be between -360 and 360 degrees.")
+
+        angle_diff = target_angle - self.angle
+        self.move_by_angle(angle_diff, speed=speed)
+
+    def get_steps(self):
+        return self.steps
+
+    def get_angle(self):
+        return self.angle
+
+    def reset_position(self):
+        self.steps = 0
+        self.angle = 0
+
+    def button_held(self):
+        print("Button held")
+        self.init_pos = True
+
+    def button_release(self):
+        print("released")
+        self.init_pos = False
+
+
+if __name__ == "__main__":
+    motor = SpinningJoints(pulse_pin=27, dir_pin=4, limit_pin=23, gear_ratio=1)
+    # motor.move_by_angle(-720*6, speed=0.5)
+    motor.init_motor(direction=1)
