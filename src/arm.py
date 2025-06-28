ba072a407f253d1cf9f1d63881591b1cafbba45c
Mikhail Romanov <mikhail.romanov@aalto.fi>
2025-06-28 21:19:15 +0300

    remade the move() so robot moves in little steps

--- src/arm.py
+++ src/arm.py
@@ -36,6 +36,10 @@ class Arm:
         self.theta_2 = None
         self.delta_r = None
 
+        self.required_theta_1 = None
+        self.required_theta_2 = None
+        self.required_delta_r = None
+
         self.iteration = 0
 
         # self.motor_paaty = SpinningJoints(pulse_pin=20, dir_pin=19, limit_pin=23, gear_ratio=1)
@@ -61,32 +65,90 @@ class Arm:
 
 
     def init_path(self):
-        self.current_path, _ = get_sun_path()
+        # self.current_path, _ = get_sun_path()
+        self.current_path = np.load("paths/test_path.npy")
         
     def move(self):
-        if (type(self.current_path) != None) and ((self.iteration - 1) < len(self.current_path)):
-            next_point = self.current_path[self.iteration-1]
-        elif ((self.iteration - 1) >= len(self.current_path)):
-            print("Robot reached the end of the path")
-            return
-        else:
-            raise ValueError("Init path first")
-        
+        if self.current_path is None:
+            raise ValueError("Initialize path first")
+
         try:
-            sols = inverse_kinematics(*next_point)
+            if self._target_not_set():
+                self._compute_next_target()
 
-            self.theta_1, self.theta_2, self.delta_r = choose_solution(sols, (self.theta_1, self.theta_2, self.delta_r))
-            self.iteration += 1
+            elif not self._step_towards('theta_1', self.required_theta_1):
+                self._check_if_new_is_safe()
+                # TODO: ADD ACTUAL MOTOR CONTROL HERE
+                pass  # Move theta_1
 
-        except:
-            self.iteration += 1
+            elif not self._step_towards('delta_r', self.required_delta_r):
+                # TODO: ADD ACTUAL MOTOR CONTROL HERE
+                self._check_if_new_is_safe()
+                pass  # Move delta_r
+
+            elif not self._step_towards('theta_2', self.required_theta_2):
+                self._check_if_new_is_safe()
+                # TODO: ADD ACTUAL MOTOR CONTROL HERE
+                pass  # Move theta_2
+
+            else:
+                self.iteration += 1
+                self._clear_target()
+
+                if self.iteration >= len(self.current_path):
+                    print("Robot reached the end of the path")
+                    return
+
+                self._compute_next_target()
+        
+        except ValueError as e:
+            print(f"Error: {e}")
+            print("Stopping robot.")
+            exit()
+
+    def _check_if_new_is_safe(self):
+        if not check_solutions_safety((self.theta_1, self.theta_2, self.delta_r)):
+            raise ValueError("New joint angles are not safe. Please adjust the path or angles.")
+
+    def _target_not_set(self):
+        return any(v is None for v in (self.required_theta_1, self.required_theta_2, self.required_delta_r))
+
+
+    def _step_towards(self, attr, target, step_size=1):
+        """Move joint 'attr' towards 'target'. Return True if reached."""
+        current = getattr(self, attr)
+        diff = target - current
+
+        if abs(diff) < step_size:
+            setattr(self, attr, target)
+            return True  # Target reached
+        else:
+            setattr(self, attr, current + step_size * (diff / abs(diff)))
+            return False  # Still moving
+
+
+    def _compute_next_target(self):
+        """Compute next target joint values based on current path."""
+        next_point = self.current_path[self.iteration]
+        sols = inverse_kinematics(*next_point)
+        self.required_theta_1, self.required_theta_2, self.required_delta_r = choose_solution(
+            sols, (self.theta_1, self.theta_2, self.delta_r)
+        )
+
+
+    def _clear_target(self):
+        """Reset required joint targets after reaching a point."""
+        self.required_theta_1 = None
+        self.required_theta_2 = None
+        self.required_delta_r = None
+
 
     def draw_all(self, index, ax):
         ax.clear()
         self.move()
         draw_all_safety_boxes(ax)
         draw_robot(ax, points=forward_kinematics(self.theta_1, self.theta_2, self.delta_r))
-        plot_path(ax, self.current_path)
+        plot_path(ax, self.current_path, linestyle='None')
 
 
 # Example usage
