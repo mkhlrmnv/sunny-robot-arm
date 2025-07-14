77de1aeef8a2c11a40817ecb922545e5bb44c962
Mikhail Romanov <mikhail.romanov@aalto.fi>
2025-07-14 10:48:29 +0300

    deleted preset as they aren't reliable and added storage of current state

--- src/lamp.py
+++ src/lamp.py
@@ -13,15 +13,24 @@ class Lamp:
         self.brightness = brightness
         self.https_url = https_url
 
+        self.red = 255
+        self.green = 255
+        self.blue = 255
+
+        self.effect = 0  # 0: solid, 1: blink, etc.
+        self.speed = 200
+
+
     def set_brightness(self, brightness, verbal=False):
         assert 0 <= brightness <= 255, "Brightness must be between 0 and 255"
-        self.brightness = brightness
 
-        url = f"{self.https_url}&A={self.brightness}"
+        url = f"{self.https_url}&A={brightness}"
         response = requests.get(url)
         
         if response.status_code != 200:
             raise Exception(f"Failed to set color: {response.status_code} - {response.text}")
+        
+        self.brightness = brightness
         
         if verbal:
             print(f"Set brightness to {self.brightness}")
@@ -39,21 +48,13 @@ class Lamp:
         if response.status_code != 200:
             raise Exception(f"Failed to set color: {response.status_code} - {response.text}")
         
+        self.red = r
+        self.green = g
+        self.blue = b
+        
         if verbal:
             print(f"Set color to R={r}, G={g}, B={b} with brightness {self.brightness}")
 
-
-    def set_preset(self, preset, verbal=False):
-        assert isinstance(preset, int), "Preset must be a integer"
-        
-        url = f"{self.https_url}&PL={preset}"
-        response = requests.get(url)
-
-        if response.status_code != 200:
-            raise Exception(f"Failed to set preset: {response.status_code} - {response.text}")
-        
-        if verbal:
-            print(f"Set preset to {preset} with brightness {self.brightness}")
 
     def set_to_effect(self, r=255, g=0, b=0, effect_index=0, speed=200, verbal=False):
         assert 0 <= r <= 255, "Red value must be between 0 and 255"
@@ -68,11 +69,18 @@ class Lamp:
         if response.status_code != 200:
             raise Exception(f"Failed to set blink: {response.status_code} - {response.text}")
         
+        self.red = r
+        self.green = g
+        self.blue = b
+        self.effect_index = effect_index
+        
         if verbal:
             print(f"Set to blink with color R={r}, G={g}, B={b}, effect index {effect_index}, speed {speed} ms")
 
+
     def set_to_solid(self, r=255, g=255, b=255, verbal=False):
         self.set_to_effect(r, g, b, effect_index=0, speed=200, verbal=verbal)
+
 
     def set_to_blink(self, r=255, g=0, b=0, speed=200, verbal=False):
         self.set_to_effect(r, g, b, effect_index=1, speed=speed, verbal=verbal)
